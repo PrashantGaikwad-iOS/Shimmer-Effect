a0571b0d22a145a5893192d77ca67fc371c19a32
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-08-06 11:11:35 +0530

    Initial Commit

---
+++ Shimmer Effect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shimmer Effect
+//
+//  Created by Prashant G on 8/6/18.
+//  Copyright © 2018 MyOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

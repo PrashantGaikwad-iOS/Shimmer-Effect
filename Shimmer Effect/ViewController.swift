ac281ed19e3c51243af22a43204130285f34f690
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-08-15 13:50:28 +0530

    Complete code

--- Shimmer Effect/ViewController.swift
+++ Shimmer Effect/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var textView1: UITextView!
+    @IBOutlet weak var startStopButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func startStopAction(_ sender: Any) {
+        
+        if startStopButton.titleLabel?.text == "START" {
+            startStopButton.setTitle("STOP", for: .normal)
+            Shimmer().startShining(label1)
+            Shimmer().startShining(view1)
+            Shimmer().startShining(textField1)
+            Shimmer().startShining(image1)
+            Shimmer().startShining(textView1)
+        }
+        else{
+            startStopButton.setTitle("START", for: .normal)
+            Shimmer().stopShining(label1)
+            Shimmer().stopShining(view1)
+            Shimmer().stopShining(textField1)
+            Shimmer().stopShining(image1)
+            Shimmer().stopShining(textView1)
+        }
+        
+    }
 }
 
